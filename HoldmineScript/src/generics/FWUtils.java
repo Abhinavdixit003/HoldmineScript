3a56461a3fe2d111e4bbd6c9a266da111cfdda64
Abhinav <abhivijdixit01@gmail.com>
2021-01-07 16:42:45 +0530

    Fwutils

--- HoldmineScript/src/generics/FWUtils.java
+++ HoldmineScript/src/generics/FWUtils.java
@@ -1,5 +1,24 @@
 package generics;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public interface FWUtils {
+	
+	public static void verifyPageTitle(WebDriver driver,String expectedTitle)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,20);
+		wait.until(ExpectedConditions.titleContains(expectedTitle));
+		String actualTitle = driver.getTitle();
+		if(actualTitle.equals(expectedTitle))
+		{
+			System.out.println("The Expected Page is Dispalyed--->"+expectedTitle);
+		}
+		else
+		{
+			System.out.println("The Expected Page is not Dispalyed--->"+actualTitle);
+		}
+	}
 
 }
